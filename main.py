128aca68baf25657da6420ee6de100ba1774c027
walquiriacoutinho <walquiriacoutinho0203@gmail.com>
2026-04-20 01:56:12 -0300

    ajuste numero randomico

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.radint(0, 20000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
